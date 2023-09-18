a982b769c6f2e67894d0b4ae1b79d185e0cdac05
Maganga Mwambonu <codebymaganga@gmail.com>
2023-09-18 23:11:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400){
+    return "This one is on me!"
+  }
+  else if (feet > 2000 && feet < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (feet > 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let checkCity = city == "NYC" ? "Ok, sounds good." : "No go.";
+  return checkCity
 }
+console.log(ternaryCheckCity("NYC"))
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip){
+    case 'generous':
+      return "Thank you so much."
+      break;
+    case 'not as generous':
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+  }   
+}
+console.log(switchOnCharmFromTip("good"))
